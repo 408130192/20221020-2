9f16c7f825aa7640abf60ef16c0f6f522954aa63
408130192 <94338644+408130192@users.noreply.github.com>
2022-10-20 16:39:16 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,236 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  background(208,193,198);
+  noFill()
+  stroke(255)
+  rectMode(CENTER)
+  strokeWeight(2)
+
+  var r = mouseX;
+
+  for(var i =0;i<width/50;i++)
+  {
+  ellipse(25+i*50,25,50)
+  rect(25+i*50,25,50)
+  ellipse(50+i*50,50,10)
+}
+  for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+ellipse(25+i*50,75,50)
+rect(25+i*50,75,50)
+ellipse(50+i*50,100,10)
+ellipse(50+i*50,100,r)  
+
+ 
+
+
+}
+
+for(var i =0;i<width/50;i++)
+  {
+  ellipse(25+i*50,25,50)
+  rect(25+i*50,25,50)
+  ellipse(50+i*50,50,10)
+}
+  for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+    
+ellipse(25+i*50,125,50)
+rect(25+i*50,125,50)
+ellipse(50+i*50,150,10)
+ellipse(50+i*50,150,r)  
+
+ 
+
+
+}
+
+for(var i =0;i<width/50;i++)
+  {
+  ellipse(25+i*50,25,50)
+  rect(25+i*50,25,50)
+  ellipse(50+i*50,50,10)
+}
+  for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+    
+ellipse(25+i*50,175,50)
+rect(25+i*50,175,50)
+ellipse(50+i*50,200,10)
+ellipse(50+i*50,200,r)  
+
+ 
+
+
+}
+for(var i =0;i<width/50;i++)
+  {
+  ellipse(25+i*50,25,50)
+  rect(25+i*50,25,50)
+  ellipse(50+i*50,50,10)
+}
+  for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+    
+ellipse(25+i*50,225,50)
+rect(25+i*50,225,50)
+ellipse(50+i*50,250,10)
+ellipse(50+i*50,250,r)  
+
+}
+
+
+for(var i =0;i<width/50;i++)
+  {
+  ellipse(25+i*50,25,50)
+  rect(25+i*50,25,50)
+  ellipse(50+i*50,50,10)
+}
+  for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+    
+ellipse(25+i*50,275,50)
+rect(25+i*50,275,50)
+ellipse(50+i*50,300,10)
+ellipse(50+i*50,300,r)  
+
+}
+
+for(var i =0;i<width/50;i++)
+  {
+  ellipse(25+i*50,25,50)
+  rect(25+i*50,25,50)
+  ellipse(50+i*50,50,10)
+}
+  for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+    
+ellipse(25+i*50,325,50)
+rect(25+i*50,325,50)
+ellipse(50+i*50,350,10)
+ellipse(50+i*50,350,r)  
+
+}
+
+for(var i =0;i<width/50;i++)
+  {
+  ellipse(25+i*50,25,50)
+  rect(25+i*50,25,50)
+  ellipse(50+i*50,50,10)
+}
+  for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+    
+ellipse(25+i*50,375,50)
+rect(25+i*50,375,50)
+ellipse(50+i*50,400,10)
+ellipse(50+i*50,400,r)  
+
+}
+
+for(var i =0;i<width/50;i++)
+  {
+  ellipse(25+i*50,25,50)
+  rect(25+i*50,25,50)
+  ellipse(50+i*50,50,10)
+}
+  for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+    
+ellipse(25+i*50,425,50)
+rect(25+i*50,425,50)
+ellipse(50+i*50,450,10)
+ellipse(50+i*50,450,r)  
+
+}
+
+for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+    
+ellipse(25+i*50,475,50)
+rect(25+i*50,475,50)
+ellipse(50+i*50,500,10)
+ellipse(50+i*50,500,r)  
+
+}
+
+for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+    
+ellipse(25+i*50,525,50)
+rect(25+i*50,525,50)
+ellipse(50+i*50,550,10)
+ellipse(50+i*50,550,r)  
+
+}
+
+
+for(var i =0;i<width/50;i++)
+  {
+  ellipse(50+i*50,50,r)
+}
+
+for(var i =0;i<width/50;i++)
+  {
+    
+ellipse(25+i*50,575,50)
+rect(25+i*50,575,50)
+ellipse(50+i*50,600,10)
+ellipse(50+i*50,600,r)  
+
+}
+
+}
